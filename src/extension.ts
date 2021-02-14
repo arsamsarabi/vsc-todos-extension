0ec4f3e7ffbe8e4f21a681ffb1a5232b04b2e50b
Arsam Sarabi <arsamsarabi@me.com>
2021-02-14 11:35:11 +0000

    🚧 sidebar boilerplate

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 import * as vscode from 'vscode'
+
 import { HelloWorldPanel } from './HelloWorldPanel'
+import { SidebarProvider } from './SidebarProvider'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "vstodos" is now active!')
+  const sidebarProvider = new SidebarProvider(context.extensionUri)
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider('vstodos-sidebar', sidebarProvider),
+  )
 
   context.subscriptions.push(
     vscode.commands.registerCommand('vstodos.helloWorld', () => {
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<h1>Sidebar</h1>
+
+<style>
+  h1 {
+    color: hotpink;
+  }
+</style>
